145006899df1a32a6547c48e6bd46835fadac8e8
Алексей Мещанов <meschanovim@mail.ru>
2023-01-13 19:47:54 +0300

    Добавили ifelse

---
+++ Example005_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.Write("Ура,это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
